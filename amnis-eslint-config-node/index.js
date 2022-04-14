906d8e10bfbaab9f2cdead8501b80aa9f70ca7eb
eCrowDev <eric.crowell@ecrow.dev>
2022-04-14 14:02:02 +0200

    feat: Initial project setup

---
+++ amnis-eslint-config-node/index.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    indent: 0,
+    'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
+    'object-curly-spacing': ['error', 'always'],
+    'space-before-blocks': ['error', 'always'],
+    '@typescript-eslint/indent': ['error', 2],
+    'no-plusplus': 0,
+    'import/extensions': [
+      'error', 'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  }
+};
